cbbc2176cdec4058c56de29f7222fda28f19fce7
Mato Estrella <matoestrella@gmail.com>
2023-01-03 11:23:23 -0300

    andando perfil sin modificar

--- src/app/Module/m-perfil/m-perfil.component.ts
+++ src/app/Module/m-perfil/m-perfil.component.ts
@@ -8,9 +8,52 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class MPerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  // Inyectar en el constructor el formBuilder
+  constructor(private formBuilder: FormBuilder){ 
+    ///Creamos el grupo de controles para el formulario de login
+    this.form= this.formBuilder.group({
+      password:['',[Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email]],
+   })
   }
 
+  ngOnInit() {}
+
+  get Password(){
+    return this.form.get("password");
+  }
+ 
+  get Mail(){
+   return this.form.get("email");
+  }
+
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return false
+  }
+ 
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+ 
+  }
+
+
 }
+ 
+
