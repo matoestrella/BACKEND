72aeea02e5d928f4537f6755d4cfd8e84b97b513
Mato Estrella <matoestrella@gmail.com>
2023-01-12 10:38:26 -0300

    antes des login

--- src/app/Module/m-esperiencia/m-esperiencia.component.ts
+++ src/app/Module/m-esperiencia/m-esperiencia.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-m-esperiencia',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MEsperienciaComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+ form: FormGroup;
+ 
+  constructor(private formBuilder: FormBuilder) { 
+    this.form= this.formBuilder.group({
+      empresa:['',[Validators.required]],
+      periodo:['', [Validators.required]],
+      descripcion:['', [Validators.required]],
+      
+    })
   }
+  ngOnInit() {}
+  
+  get Empresa(){
+    return this.form.get("empresa");
+   }
+   get Periodo(){
+    return this.form.get("periodo");
+   }
+
+  get Descripcion(){
+    return this.form.get("descripcion");
+  }
+ 
+ 
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+ 
+  } 
 
 }
